ca6e553ee5bba39fa8e700322fca8b62a25d1144
thomasvld <thomas.valade@viacesi.fr>
2024-05-14 15:58:51 +0200

    Modif fichier pour relier l'API WEB

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -4,7 +4,7 @@ import 'package:http/http.dart' as http;
 
 class ApiService {
   Future<List<ArticleModel>> getArticles() async {
-    final response = await http.get(Uri.http("http://localhost:3000", "Image"));
+    final response = await http.get(Uri.http("localhost:3000", "controllers"));
     if (response.statusCode == 200) {
       List<dynamic> articlesJson = json.decode(response.body);
       List<ArticleModel> articles =
